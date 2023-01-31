2ad39c43468f336bbe737c6ad8a7a368258e4fc5
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-01-31 13:05:38 +0200

    (feat) - add function update and delete

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -24,17 +24,25 @@ class PostController {
     }
     async updatePost(req, res) {
         try {
-            const posts = await Post.find()
-            return res.status(200).json()
+            const {id, title, text} = req.body
+            const post = await Post.update({id}, {title, text})
+            if(!post) {
+               return res.status(400).json({message: 'Такого поста не існує.'})
+            }
+            return res.status(200).json({message: `Пост id: ${id} оновлено.`})
         } catch (error) {
             console.log(error.message)
-            return res.status(500).json({message: 'Помилка при поверненні списка користувачів.'})
+            return res.status(500).json({message: 'Помилка при створенні поста.'})
         }
     }
     async deletePost(req, res) {
         try {
-            const posts = await Post.find()
-            return res.status(200).json()
+            const {id} = req.body
+            const post = await Post.remove({id})
+            if(!post) {
+                return res.status(400).json({message: 'Такого поста не існує.'})
+            }
+            return res.status(200).json({message: `Пост id: ${id} видалено.`})
         } catch (error) {
             console.log(error.message)
             return res.status(500).json({message: 'Помилка при поверненні списка користувачів.'})
