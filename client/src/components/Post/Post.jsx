4caeddccb1c54a0342540ffc00a10e99f91ab424
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2023-02-01 16:59:50 +0200

    (feat) - add modal

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -4,27 +4,43 @@ import classes from './Post.module.sass'
 import {useMessage} from "../../hooks/message.hook";
 import {useDispatch} from "react-redux";
 import {removeOnePost} from "../../redux/postSlice";
+import Modal from "../Modal/Modal";
 
 const Post = ({id, title, text}) => {
     const {request, error, clearError, loading} = useHttp()
-    const [postId, setPostId] = useState(id)
+    const [postId] = useState(id)
     const message = useMessage()
     const dispatch = useDispatch()
 
-    useEffect(()=> {
+    useEffect(() => {
+        window.M.AutoInit()
+    }, [])
+
+    useEffect(() => {
         message(error)
         clearError()
-    },[error, clearError])
+    }, [error, clearError])
 
     const deleteHandler = async () => {
         try {
             const response = await request('/api/posts', 'DELETE', {id: postId})
-            if(response.isDelete) {
+            if (response.isDelete) {
                 message(response.message)
                 dispatch(removeOnePost(postId))
             }
         } catch (e) {}
     }
+
+    const updateHandler = async () => {
+        try {
+            // const response = await request('/api/posts', 'PATCH', {id: postId, title: '', text: ''})
+            // if (response.isUpdate) {
+            //     message(response.message)
+            //     dispatch(removeOnePost(postId))
+            // }
+        } catch (e) {}
+    }
+
     return (
         <div className='card'>
             <div className="card-content">
@@ -32,9 +48,23 @@ const Post = ({id, title, text}) => {
                 <div>{text}</div>
             </div>
             <div className="card-action">
-                <button className={`btn ${classes.btn}`} onClick={deleteHandler}>Delete</button>
-                <button className={`btn ${classes.btn}`}>Update</button>
+                <button
+                    className={`btn ${classes.btn}`}
+                    onClick={deleteHandler}
+                    disabled={loading}
+                >
+                    Delete
+                </button>
+                <button
+                    data-target="modal1"
+                    className={`btn modal-trigger ${classes.btn}`}
+                    onClick={updateHandler}
+                    disabled={loading}
+                >
+                    Update
+                </button>
             </div>
+            <Modal/>
         </div>
     );
 };
